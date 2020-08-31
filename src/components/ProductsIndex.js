1428eec91518b8bedb8d113f3cc3c6684912e276
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-31 20:29:59 +0500

    Update ProductsIndex.js

--- src/components/ProductsIndex.js
+++ src/components/ProductsIndex.js
@@ -1,51 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
+import Case1 from './Case1';
+import { Link, Routes, Route } from 'react-router-dom';
+
 
 
 function ProductsIndex() {
 
-    const shoes = {
 
-        "Dunham-Mens-8000-Ubal-Waterproof-Shoes" : {
-            name: "Dunham Mens 8000 Ubal Waterproof Lace Up Sneaker Shoes",
-            img: "https://ak1.ostkcdn.com/images/products/30756882/Dunham-Mens-8000-Ubal-Waterproof-Lace-Up-Sneaker-Shoes-72fe64fd-778e-4e69-a9a8-ce3d254cfc42.jpg",
-            detail: "This is the detail of the product"
-        },
-        "Lugz-Men-Zrocs-DX-Oxford-Sneaker" : {
-            name: "Lugz Men's Zrocs DX Oxford Sneaker",
-            img: "https://ak1.ostkcdn.com/images/products/22897350/Zrocs-DX-42c19146-ec9d-4925-9868-abd8a0aa2bb3.jpg",
-            detail: "This is the detail of the product"
-        },
-        "Vance-Co.-Men-Riggin-Athleisure-Elastic-Sneakers" : {
-            name: "Vance Co. Men's 'Riggin' Athleisure Elastic Quick Lace Casual Sneakers",
-            img: "https://ak1.ostkcdn.com/images/products/17011753/Vance-Co.-Mens-Riggin-Athleisure-Elastic-Quick-Lace-Casual-Sneakers-03572354-28ed-4fab-a881-96fb6c35a8f6_600.jpg",
-            detail: "This is the detail of the product"
-        },
-        "Prince-Mens-NFS-Attack-Squash-Sneakers" : {
-            name: "Prince Mens NFS Attack Squash Sneakers",
-            img: "https://ak1.ostkcdn.com/images/products/19892982/Prince-Mens-NFS-Attack-Squash-Sneakers-493dd13e-19e3-427b-a07b-3313483d7570_600.jpg",
-            detail: "This is the detail of the product"
-        },
-
-    }
 
     return (
         <div>
-                                    
-           
-           <ul>
-               {Object.entries(shoes).map(([productID, { name, img }]) => {
-                   return (
-                       <li key={productID}>
-                           <Link to={productID}>
-                               <h2> {name}</h2>
-                               <img src={img} alt={name} style={{height: '300px', width: '300px'}} />
-                           </Link>
-                       </li>
-                   )
-               })}
-          
-           </ul>
+
+                   <h1> Products </h1>
+            
+            <Link to='Case1'> Case1 </Link> <br />
+            <Link to='Case2'> Case2 </Link> <br />
+            <Link to='Case3'> Case3 </Link> <br />
+            <Link to='Case4'> Case4 </Link> 
+                       
+                    
+
 
         </div>
     )
