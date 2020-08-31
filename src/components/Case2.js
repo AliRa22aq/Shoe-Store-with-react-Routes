6dc8ae1d7fecb09451f345d64f3b7733e068370d
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-31 20:29:31 +0500

    Create Case2.js

---
+++ src/components/Case2.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+
+const Girls = {
+
+    "Dunham-Mens-8000-Ubal-Waterproof-Shoes" : {
+        name: "Dunham Mens 8000 Ubal Waterproof Lace Up Sneaker Shoes",
+        img: "https://ak1.ostkcdn.com/images/products/30756882/Dunham-Mens-8000-Ubal-Waterproof-Lace-Up-Sneaker-Shoes-72fe64fd-778e-4e69-a9a8-ce3d254cfc42.jpg",
+        detail: "This is the detail of the product"
+    },
+    "Lugz-Men-Zrocs-DX-Oxford-Sneaker" : {
+        name: "Lugz Men's Zrocs DX Oxford Sneaker",
+        img: "https://ak1.ostkcdn.com/images/products/22897350/Zrocs-DX-42c19146-ec9d-4925-9868-abd8a0aa2bb3.jpg",
+        detail: "This is the detail of the product"
+    },
+    "Vance-Co.-Men-Riggin-Athleisure-Elastic-Sneakers" : {
+        name: "Vance Co. Men's 'Riggin' Athleisure Elastic Quick Lace Casual Sneakers",
+        img: "https://ak1.ostkcdn.com/images/products/17011753/Vance-Co.-Mens-Riggin-Athleisure-Elastic-Quick-Lace-Casual-Sneakers-03572354-28ed-4fab-a881-96fb6c35a8f6_600.jpg",
+        detail: "This is the detail of the product"
+    },
+    "Prince-Mens-NFS-Attack-Squash-Sneakers" : {
+        name: "Prince Mens NFS Attack Squash Sneakers",
+        img: "https://ak1.ostkcdn.com/images/products/19892982/Prince-Mens-NFS-Attack-Squash-Sneakers-493dd13e-19e3-427b-a07b-3313483d7570_600.jpg",
+        detail: "This is the detail of the product"
+    },
+
+}
+
+function Case2() {
+    return (
+        <div>
+            Girls Shoes
+            <ul> 
+            {Object.entries(Girls).map(([productID,{name, img}]) => {
+                return(
+
+                    <li key={productID}> 
+                                {name}
+                        <img src={img} alt={name} /> 
+                    </li>
+
+                )
+            })}
+            </ul>
+
+        </div>
+    )
+}
+
+export default Case2
