7870ca78df32480f46b61c61770d27d82296ccb9
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-02 22:28:26 +0500

    Update ProductsIndex.js

--- src/components/ProductsIndex.js
+++ src/components/ProductsIndex.js
@@ -1,92 +1,152 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
+
 import { Link } from 'react-router-dom';
 
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+
+const images = [
+  {
+      caseNo: 1,
+    case: 'case1',
+    url: 'https://media2.giphy.com/media/l3q2rCBSrr6D7XKLK/giphy.gif',
+    title: 'Boys Shoes',
+    width: '50%',
+  },
+  {
+    case: 'case2',
+    url: 'https://media2.giphy.com/media/l3q2rCBSrr6D7XKLK/giphy.gif',
+    title: 'Girls Shoes',
+    width: '50%',
+  },
+  {
+    case: 'case3',
+    url: 'https://media2.giphy.com/media/l3q2rCBSrr6D7XKLK/giphy.gif',
+    title: 'Kids Shoes',
+    width: '50%',
+  },
+  {
+    case: 'case4',
+    url: 'https://media2.giphy.com/media/l3q2rCBSrr6D7XKLK/giphy.gif',
+    title: 'Accessaries',
+    width: '50%',
+  },
+
+
+];
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
+    flexWrap: 'wrap',
+    minWidth: 300,
+    width: '100%',
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+  image: {
+    position: 'relative',
+    height: 250,
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100,
+    },
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15,
+      },
+      '& $imageMarked': {
+        opacity: 0,
+      },
+      '& $imageTitle': {
+        border: '4px solid currentColor',
+      },
+    },
+  },
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  },
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
   },
 }));
 
-
-function ProductsIndex() {
-
+export default function PictureButton() {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
+      {images.map((image) => (
+          
+        <ButtonBase
+          focusRipple
+          key={image.title}
+          className={classes.image}
+          focusVisibleClassName={classes.focusVisible}
+          style={{
+            width: image.width,
+          }}
+        >
+            <Link to={image.case}> 
+          <span
+            className={classes.imageSrc}
+            style={{
+                backgroundImage: `url(${image.url})`
+            }}
+          />
+          <span className={classes.imageBackdrop} />
+          <span className={classes.imageButton}>
+           <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              className={classes.imageTitle}
+            >
+               {image.title}         
 
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>  <h1> Products </h1> </Paper>
-          </Grid>
-
-
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/c/c8/New_Balance_574_shoe.gif"
-                alt={"Nothing"}
-                width={200}
-                height={200} />
-              <br />
-              <Link to='Case1'> Men Shoes </Link> <br />
-            </Paper>
-          </Grid>
-
-
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <img
-                src="https://cdn.shopify.com/s/files/1/0070/0497/9264/products/KV128326_0011_10_grande.gif?v=1588240501"
-                alt={"Nothing"}
-                width={200}
-                height={200} />
-              <br />
-              <Link to='Case2'> Women Shoes </Link> <br />
-            </Paper>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <img
-                src="https://media2.giphy.com/media/l3q2rCBSrr6D7XKLK/giphy.gif"
-                alt={"Nothing"}
-                width={200}
-                height={200} />
-              <br />
-              <Link to='Case3'> Kids Shoes </Link> <br />
-            </Paper>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <img
-                src="https://ak1.ostkcdn.com/images/products/30756882/Dunham-Mens-8000-Ubal-Waterproof-Lace-Up-Sneaker-Shoes-72fe64fd-778e-4e69-a9a8-ce3d254cfc42.jpg"
-                alt={"Nothing"}
-                width={200}
-                height={200} />
-              <br />
-              <Link to='Case4'> Accessories </Link>
-            </Paper>
-          </Grid>
-        </Grid>
-
-      </div>
-
+              <span className={classes.imageMarked} />
+            </Typography>
+          </span></Link>
+        </ButtonBase> 
+      ))}
     </div>
-  )
+  );
 }
-
-export default ProductsIndex
-
-
