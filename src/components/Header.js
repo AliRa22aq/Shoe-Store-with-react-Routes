cba748e00b94b9898cc23ea44559f7a2f0995224
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-02 23:40:54 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,15 @@ import '../App.css';
 
 function Header() {
     return (
-        <nav className = 'navbar'>
-            <NavLink to="/" as='li'> Home </NavLink> |
+        <nav className = 'navbar' >
+            <div className = 'header'> 
+            <NavLink to="/" as='li' >  Home  </NavLink> |
             <NavLink to="/Products" as='li'> Products </NavLink> |
+            <NavLink to="/Products/Case1" as='li'> Boys </NavLink> |
+            <NavLink to="/Products/Case2" as='li'> Girls </NavLink> |
+            <NavLink to="/Products/Case2" as='li'> Kids </NavLink> |
             <NavLink to="/About" as='li'> About Developer </NavLink>
+            </div>
         </nav>
     )
 }
