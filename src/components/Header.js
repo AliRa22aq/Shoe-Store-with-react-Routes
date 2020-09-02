cd558c159d9b51efe094cf56c59ef0d8113504e1
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-03 00:28:52 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import '../App.css';
+import { Grid } from '@material-ui/core';
+import LOGO from './url/LOGO.jpg'
 
 function Header() {
     return (
+        <Grid container>
+            <Grid item xs={1} className = 'headerupper'> 
+                <Link to='/'> 
+                    <img src={LOGO} alt="Shoe" height="70" width="90" /> 
+                </Link>
+            </Grid>
+
+            <Grid item xs={10} className = 'headerupper'> THE SHOE STORE</Grid>
+            
+            <Grid item xs={12} className = 'header'> 
         <nav className = 'navbar' >
-            <div className = 'header'> 
+            <div > 
             <NavLink to="/" as='li' >  Home  </NavLink> |
             <NavLink to="/Products" as='li'> Products </NavLink> |
             <NavLink to="/Products/Case1" as='li'> Boys </NavLink> |
@@ -14,6 +26,8 @@ function Header() {
             <NavLink to="/About" as='li'> About Developer </NavLink>
             </div>
         </nav>
+        </Grid>
+        </Grid>
     )
 }
 
