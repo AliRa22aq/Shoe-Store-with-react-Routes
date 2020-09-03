8c8fee1cd1835bfea3b13313655efeb51f135a26
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-04 00:57:21 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,10 +11,10 @@ function Home() {
 
             <Grid container > 
             <Grid item xs={3} className='writing'> 
-            A place Where you Fall in Love with Shoes
-            <Button variant="outlined" > Products </Button>
+            A place Where you Fall in Love with Shoes <br />
+            <Button variant="outlined" className='button' > Products </Button>
             </Grid>
-            <Grid item xs={9}> <img src={image}  height={600} width={920} className='mainImage'/>   </Grid>
+            <Grid item xs={9}> <img src={image}  height={600} width={1010} />   </Grid>
             </Grid>
             
         </div>
