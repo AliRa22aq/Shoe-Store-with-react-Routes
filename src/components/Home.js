22b1666d58b1fdecefc85aaa87d16c80710f6d67
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-04 00:40:03 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import image from './url/comp-1.gif'
+import Button from '@material-ui/core/Button';
+
+
 
 function Home() {
     return (
-        <div>
-           <h1>Welcome to Home</h1>
+        <div >
+
+            <Grid container > 
+            <Grid item xs={3} className='writing'> 
+            A place Where you Fall in Love with Shoes
+            <Button variant="outlined" > Products </Button>
+            </Grid>
+            <Grid item xs={9}> <img src={image}  height={600} width={920} className='mainImage'/>   </Grid>
+            </Grid>
+            
         </div>
     )
 }
