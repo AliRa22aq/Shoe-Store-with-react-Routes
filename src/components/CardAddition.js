3ba02144889c621ed8f12e39fa40831a3a43a23b
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-05 02:40:30 +0500

    Create CardAddition.js

---
+++ src/components/CardAddition.js
@@ -0,0 +1,16 @@
+import React, {useContext} from 'react'
+import {CartContext} from './CartContext';
+
+export const CardAddition = (x) => {
+
+    ///const [C, setC] = useContext(CartContext);
+
+    console.log(x);
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
