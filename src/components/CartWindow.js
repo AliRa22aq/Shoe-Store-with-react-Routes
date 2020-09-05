fca7f7c042ed68bb7a05ab403ea070db63adf89d
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-06 02:37:26 +0500

    Update CartWindow.js

--- src/components/CartWindow.js
+++ src/components/CartWindow.js
@@ -1,43 +1,77 @@
 import React, {useState, useContext} from 'react'
 import {CartContext} from './CartContext';
-import { Button } from '@material-ui/core';
+import { Button, Grid, Paper, Link } from '@material-ui/core';
+import '../App.css';
+
 
 
 
 function CartWindow() {
     const [Cart, setCart] = useContext(CartContext);
-    const [num, setnum] = useState({})
 
     var priceData = Cart.map((item) => {return( item.price)})
     var TotalPrice = priceData.reduce((acc, curr) => acc+curr, 0);
+
+    console.log(Cart)
     
         
     return (
         <div>
-            <h4> Products added into Cart</h4>
-            <ul> 
+
+            <Grid className = 'detailhead' container> <h4> Products added into Cart </h4> </Grid>
+
+            <ol> 
+             
             {Cart.map((val) => {
                 return (
+                    <Paper>
+                    <li> 
+                     <Grid container>   
+                     <Grid item xs={4 } className='carttext'> 
+                        Name of the Product: {val.name}  <br />
+                        
+                        </Grid> 
+                        
+                        <Grid item xs={5 }> 
+                        <img src={val.image} alt={val.name} width={300} height={300} />
+                        </Grid> 
                     
-                    <li> {val.name} | |{val.price}    <br />
 
+                    <Grid item xs={3 }> 
+                    <Grid container > 
+                    <div  className= 'cartprice'>   Amount: ${val.price}  </div>
+                    </Grid>
                     
+                    <div className='cartbutton'>
                     <Button variant="outlined" onClick={() => {setCart(Cart.filter((a) => {return ( a.name!=val.name)} ))}} > 
                     Delete from Cart 
                     </Button>
+                    </div>
+                    </Grid > 
                         
                         
                         
-                        
-                    </li> 
+                    </Grid>
+                    </li>
+                    </Paper>
                     )  
                     
             } ) }
-         Total Bill: {TotalPrice}
-            </ul>
             
+            </ol>
 
+            <Paper > 
+            <Grid className='carttotal' item xs={12}> Total Bill: ${TotalPrice} </Grid>     
             
+            <div className='checkout' >    
+            
+  
+            <Grid item xs={11}>   </Grid>   
+            <Grid item xs={1}> <Button> Check Out </Button>  </Grid>   
+
+
+            </div>
+           </Paper>
 
         </div>
     )
