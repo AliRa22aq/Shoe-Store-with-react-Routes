7d8765bb1fa2cd99f70bc122882e3cab2c2f3114
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-05 22:03:06 +0500

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,25 @@
 import React, {useContext} from 'react'
 import {CartContext} from './CartContext'
+import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
+
 
 function Cart() {
 
     const [Cart, setCart] = useContext(CartContext);
 
+
+    var priceData = Cart.map((item) => {
+        return( item.price)
+    })
+
+    const totalPrice = priceData.reduce((acc, curr) => acc+curr, 0);
+
     return (
-        <div>
-            total items added: {Cart.length} <br/>
-            total price: $
+        <div style={{fontSize: 15}} >
+            <LocalGroceryStoreIcon/> {'('} {Cart.length} {')'} | ${totalPrice}
         </div>
     )
+
 }
 
 export default Cart
