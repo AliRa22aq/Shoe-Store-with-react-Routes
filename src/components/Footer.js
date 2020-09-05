0d2e88c41984757c80289839618953f0d03643ed
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-06 02:50:04 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
-function Footer() {
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        width: '100%',
+        marginTop: '5px',
+        marginBottom: '5px',
+        textAlign: 'center',
+    },
+    footer: {
+        padding: theme.spacing(3, 2),
+        backgroundColor: 'white'
+    },
+    linkColor: {
+        color: 'white',
+    },
+}));
+
+
+const Footer = () => {
+    const classes = useStyles();
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <div className={classes.container}>
+            <footer className={classes.footer}>
+                <Container maxWidth="sm">
+                    <Typography variant="body1" color="primary">
+                        
+                        <Link className={classes.linkColor} href="https://github.com/AliRa22aq" target="_blank" rel="noopener noreferrer">
+                        Created by: {' '}Ali Razzaq {'  '} <GitHubIcon />
+                        </Link> 
+                    </Typography>
 
-export default Footer
+                </Container>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
