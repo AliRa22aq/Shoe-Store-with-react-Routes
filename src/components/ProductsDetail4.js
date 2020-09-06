1541c916c43348552db0fdd18e19a84b9f7313de
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-06 16:59:36 +0500

    Update ProductsDetail4.js

--- src/components/ProductsDetail4.js
+++ src/components/ProductsDetail4.js
@@ -1,57 +1,93 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { Card } from '@material-ui/core';
-import '../App.css';
 
-
-function ProductsDetail4() {
-
-    const Accessaries = {
-
-        "Dunham-Mens-8000-Ubal-Waterproof-Shoes" : {
-            name: "Dunham Mens 8000 Ubal Waterproof Lace Up Sneaker Shoes",
-            img: "https://ak1.ostkcdn.com/images/products/30756882/Dunham-Mens-8000-Ubal-Waterproof-Lace-Up-Sneaker-Shoes-72fe64fd-778e-4e69-a9a8-ce3d254cfc42.jpg",
-            detail: "This is the detail of the product  11111"
-        },
-        "Lugz-Men-Zrocs-DX-Oxford-Sneaker" : {
-            name: "Lugz Men's Zrocs DX Oxford Sneaker",
-            img: "https://ak1.ostkcdn.com/images/products/22897350/Zrocs-DX-42c19146-ec9d-4925-9868-abd8a0aa2bb3.jpg",
-            detail: "This is the detail of the product    2222"
-        },
-        "Vance-Co.-Men-Riggin-Athleisure-Elastic-Sneakers" : {
-            name: "Vance Co. Men's 'Riggin' Athleisure Elastic Quick Lace Casual Sneakers",
-            img: "https://ak1.ostkcdn.com/images/products/17011753/Vance-Co.-Mens-Riggin-Athleisure-Elastic-Quick-Lace-Casual-Sneakers-03572354-28ed-4fab-a881-96fb6c35a8f6_600.jpg",
-            detail: "This is the detail of the product   3333"
-        },
-        "Prince-Mens-NFS-Attack-Squash-Sneakers" : {
-            name: "Prince Mens NFS Attack Squash Sneakers",
-            img: "https://ak1.ostkcdn.com/images/products/19892982/Prince-Mens-NFS-Attack-Squash-Sneakers-493dd13e-19e3-427b-a07b-3313483d7570_600.jpg",
-            detail: "This is the detail of the product 44444"
-        },
-
-    }
-
-    const {product4ID} = useParams();
-
-    const product = Accessaries[product4ID];
-    const {name, img, detail} = product;
+    import React, {useState, useContext} from 'react'
+    import {CartContext} from './CartContext';
+    import { useParams } from 'react-router-dom'
+    import { Card } from '@material-ui/core';
+    import { Grid, Button } from '@material-ui/core';
+    import '../App.css';
+    import Typography from '@material-ui/core/Typography';
     
+    function ProductsDetail4() {
+    
+        const [Cart, setCart] = useContext(CartContext);
+    
+        const Socks = {
 
-    return (
-        <div>
-            <ul> 
-            <li>  
-            <Card >
-                    <h4> {name} </h4>
-                    <p> {detail}</p>
-                    <img src={img} alt={name} style={{height: 200, width: '200px'}}/>
-                    </Card> 
-                   
-            </li>
-            </ul>
-            </div>
+            "Men-Nike-Lightweight" : {
+                name: "Men Nike Lightweight",
+                img: "https://www.aeelectricalinc.com/images/large/Fashion_Style/Men%20Nike%20Lightweight%203%20Pack%20Sock%20Black%20White%20Grey%20-%20Nike%20Men%20Socks%20Sale%20C33b3409%20138_LRG.jpg",
+                detail: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                price: 100
 
-    )
-}
+            },
+            "Woowan-DX-Oxfor-Socks" : {
+                name: "Woowan DX Oxford Socks",
+                img: "https://ae01.alicdn.com/kf/HTB1lGKFOpXXXXaZaXXXq6xXFXXXz/Men-Sock-10-pieces-5-Pairs-lot-Package-Male-Summer-Light-Socks-Stripe-Cotton-Short-Sock.jpg_480x480xz.jpg",
+                detail: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                price: 100
 
-export default ProductsDetail4
+            },
+            "Nik-Trainings-most-basic" : {
+                name: "Nike Training's most basic",
+                img: "https://cache.mrporter.com/variants/images/5983760398201134/in/w2000_q80.jpg",
+                detail: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                price: 100
+            },
+
+        }
+    
+        const {product4ID} = useParams();
+    
+        const product = Socks[product4ID];
+        const {name, img, price, detail} = product;
+        
+    
+        return (
+            <div>
+    
+    <Card >
+    
+        <Grid container >  
+    
+                       <Grid className = 'detailhead' container> <h4> {name} </h4> </Grid>
+                     
+    
+                       <Grid className='detalphoto' item xs = {6}> <img src={img} alt={name} style={{height: 500, width: 500}}/></Grid>
+    
+    
+                       <Grid  item xs = {6} > 
+    
+                        
+                       
+                       <Grid item xs = {12}> 
+                       <Typography className='detail' variant="subtitle1" gutterBottom>
+                           
+                         {detail}    
+    
+                        </Typography>
+                        <div className = 'ShoePrice'>  
+                                ${price}
+                        </div>
+    
+                        <div className = 'button'> 
+                        <Button variant="outlined" onClick = {() => {setCart(a => [...a, {"name": name, 'price': price}])}} > 
+                         Add to Cart
+                        </Button>
+                        </div>
+    
+                        </Grid>
+                      
+                       </Grid> 
+                       
+                      
+    
+        </Grid>
+     </Card> 
+    
+                </div>
+    
+        )
+    }
+    
+    export default ProductsDetail4
+      
