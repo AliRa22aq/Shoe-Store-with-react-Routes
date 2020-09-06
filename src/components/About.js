b07296bc548bf04fc382ba7adda182279e7f0b57
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-06 17:02:27 +0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Button } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import profile from './url/profile.jpg'
 import Typography from '@material-ui/core/Typography';
 
