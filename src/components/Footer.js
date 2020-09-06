0ecb3c76c4c750e6c3134dbca962173fc4145e0f
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-06 19:05:54 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -30,7 +31,7 @@ const Footer = () => {
                     <Typography variant="body1" color="primary">
                         Created by:{' '}
                         <Link className={classes.linkColor} href="https://github.com/AliRa22aq" target="_blank" rel="noopener noreferrer">
-                         Ali Razzaq {'  '} 
+                         Ali Razzaq {'  '} <GitHubIcon />
                         </Link> 
                     </Typography>
 
