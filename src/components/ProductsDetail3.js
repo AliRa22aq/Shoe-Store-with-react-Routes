6a329b508656f506d15aea89692f108c9e65271f
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-06 16:59:33 +0500

    Update ProductsDetail3.js

--- src/components/ProductsDetail3.js
+++ src/components/ProductsDetail3.js
@@ -1,58 +1,102 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { Card } from '@material-ui/core';
-import '../App.css';
 
-
-function ProductsDetail3() {
-
-    const Kshoes = {
-
-        "Dunham-Mens-8000-Ubal-Waterproof-Shoes" : {
-            name: "Dunham Mens 8000 Ubal Waterproof Lace Up Sneaker Shoes",
-            img: "https://ak1.ostkcdn.com/images/products/30756882/Dunham-Mens-8000-Ubal-Waterproof-Lace-Up-Sneaker-Shoes-72fe64fd-778e-4e69-a9a8-ce3d254cfc42.jpg",
-            detail: "This is the detail of the product  11111"
-        },
-        "Lugz-Men-Zrocs-DX-Oxford-Sneaker" : {
-            name: "Lugz Men's Zrocs DX Oxford Sneaker",
-            img: "https://ak1.ostkcdn.com/images/products/22897350/Zrocs-DX-42c19146-ec9d-4925-9868-abd8a0aa2bb3.jpg",
-            detail: "This is the detail of the product    2222"
-        },
-        "Vance-Co.-Men-Riggin-Athleisure-Elastic-Sneakers" : {
-            name: "Vance Co. Men's 'Riggin' Athleisure Elastic Quick Lace Casual Sneakers",
-            img: "https://ak1.ostkcdn.com/images/products/17011753/Vance-Co.-Mens-Riggin-Athleisure-Elastic-Quick-Lace-Casual-Sneakers-03572354-28ed-4fab-a881-96fb6c35a8f6_600.jpg",
-            detail: "This is the detail of the product   3333"
-        },
-        "Prince-Mens-NFS-Attack-Squash-Sneakers" : {
-            name: "Prince Mens NFS Attack Squash Sneakers",
-            img: "https://ak1.ostkcdn.com/images/products/19892982/Prince-Mens-NFS-Attack-Squash-Sneakers-493dd13e-19e3-427b-a07b-3313483d7570_600.jpg",
-            detail: "This is the detail of the product 44444"
-        },
-
-    }
-
-
-    const {product3ID} = useParams();
-
-    const product = Kshoes[product3ID];
-    const {name, img, detail} = product;
+    import React, {useState, useContext} from 'react'
+    import {CartContext} from './CartContext';
+    import { useParams } from 'react-router-dom'
+    import { Card } from '@material-ui/core';
+    import { Grid, Button } from '@material-ui/core';
+    import '../App.css';
+    import Typography from '@material-ui/core/Typography';
     
+    function ProductsDetail3() {
+    
+        const [Cart, setCart] = useContext(CartContext);
+    
+        const Kshoes = {
 
-    return (
-        <div>
-            <ul> 
-            <li>  
-            <Card >
-                    <h4> {name} </h4>
-                    <p> {detail}</p>
-                    <img src={img} alt={name} style={{height: 200, width: '200px'}}/>
-                    </Card> 
-                   
-            </li>
-            </ul>
-            </div>
+            "Girls-Sports-Shoe-301-Red" : {
+                name: "Sports Shoes 301 - Red",
+                img: "https://cdn.shopify.com/s/files/1/0081/3504/9293/products/301_2_480x.jpg?v=1578487460",
+                detail: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                price: 100
+        
+            },
+            "Sports-Shoes-102-Green" : {
+                name: "Girls Sports Shoes 102 - Sea Green",
+                img: "https://cdn.shopify.com/s/files/1/0081/3504/9293/products/102_1_650d757d-6059-4ffa-a128-92d83bfd9bcc_480x.jpg?v=1578487002",
+                detail: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                price: 100
+        
+            },
+            "Girls-Sports-Shoes-102-Sky-Blue" : {
+                name: "Girls Sports Shoes 102 - Sky Blue",
+                img: "https://cdn.shopify.com/s/files/1/0081/3504/9293/products/102_2_cb728a20-f611-4be2-bfa1-2c736ff843ae_480x.jpg?v=1578487200",
+                detail: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                price: 100
+        
+            },
+            "Comfortable-Black-Slippers-For-Kids" : {
+                name: "Comfortable Black Slippers For Kids",
+                img: "https://d11zer3aoz69xt.cloudfront.net/media/catalog/product/cache/1/image/1200x/9df78eab33525d08d6e5fb8d27136e95/a/s/asaan_buy_comfortable_black_slippers_for_kids_to-0017-y_.jpg",
+                detail: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                price: 100
+        
+            },
+        
+        }
+        
 
-    )
-}
-
-export default ProductsDetail3
+        const {product3ID} = useParams();
+    
+        const product = Kshoes[product3ID];
+        const {name, img, price, detail} = product;
+        
+    
+        return (
+            <div>
+    
+    <Card >
+    
+        <Grid container >  
+    
+                       <Grid className = 'detailhead' container> <h4> {name} </h4> </Grid>
+                     
+    
+                       <Grid className='detalphoto' item xs = {6}> <img src={img} alt={name} style={{height: 500, width: 500}}/></Grid>
+    
+    
+                       <Grid  item xs = {6} > 
+    
+                        
+                       
+                       <Grid item xs = {12}> 
+                       <Typography className='detail' variant="subtitle1" gutterBottom>
+                           
+                         {detail}    
+    
+                        </Typography>
+                        <div className = 'ShoePrice'>  
+                                ${price}
+                        </div>
+    
+                        <div className = 'button'> 
+                        <Button variant="outlined" onClick = {() => {setCart(a => [...a, {"name": name, 'price': price}])}} > 
+                         Add to Cart
+                        </Button>
+                        </div>
+    
+                        </Grid>
+                      
+                       </Grid> 
+                       
+                      
+    
+        </Grid>
+     </Card> 
+    
+                </div>
+    
+        )
+    }
+    
+    export default ProductsDetail3
+    
