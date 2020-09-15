ffece5136afef30b06ba8c3df8c5b02a9d045469
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-16 00:32:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ import Case4 from './components/Case4';
 import NotFound from './components/NotFound';
 import ProductsIndex from './components/ProductsIndex';
 import About from './components/About';
-import { CartProvider } from './components/CartContext';
+import { GlobalProvider } from './components/CartContext';
 import CartWindow from './components/CartWindow';
 import Footer from './components/Footer'
 
@@ -30,11 +30,12 @@ import Footer from './components/Footer'
 function App() {
   return (
 
- 
     <div>
 
-<CartProvider >
+<GlobalProvider >
+
       <Header />
+
       <Routes> 
   
         <Route path="/About" element={<About />} />      
@@ -42,25 +43,29 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="Products" element={<Products />}> 
            <Route path="/" element={<ProductsIndex />} /> 
+           
            <Route path="/Case1" element={<Case1 />} />
            <Route path="/Case1/:product1ID" element={<ProductsDetail1 />} />
+
            <Route path="/Case2" element={<Case2 />} />
            <Route path="/Case2/:product2ID" element={<ProductsDetail2 />} />
+
            <Route path="/Case3" element={<Case3 />} />
            <Route path="/Case3/:product3ID" element={<ProductsDetail3 />} />
+
            <Route path="/Case4" element={<Case4 />} />
            <Route path="/Case4/:product4ID" element={<ProductsDetail4 />} />
+
         </Route>
         <Route path="*" element={<NotFound />} />
         
 
       </Routes>
       <Footer />
-
-      
-      </CartProvider>
+      </GlobalProvider>
 
     </div>
+    
   );
 }
 
